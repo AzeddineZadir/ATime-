0face8928cba6b5768c2b956d92eb3dffa8d6a4f
djamil <maouchi-djamil@outlook.fr>
2020-04-24 16:10:52 +0200

    Add new folder apps

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pointage',
+    'apps.pointage',
 ]
 
 MIDDLEWARE = [
--- project/project/urls.py
+++ project/project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pointage/', include('pointage.urls')),
+    path('pointage/', include('apps.pointage.urls')),
 ]
