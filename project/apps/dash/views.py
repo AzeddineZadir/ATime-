3764a3d18d6f613083f74cb78d65818e6df66640
silvermoon06 <silvermoon06000@gmail.com>
2020-04-29 00:27:25 +0100

    add docorators to protect the dashboards

--- project/apps/dash/views.py
+++ project/apps/dash/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
+from .decorators import employe_required, manger_required, project_manger_required
 
 # Create your views here.
 
 
+@employe_required
 def dash_emp(request):
     return render(request, 'dash/dash_emp.html')
 
 
+@manger_required
 def dash_man(request):
     return render(request, 'dash/dash_man.html')
 
 
+@project_manger_required
 def dash_pro_man(request):
     return render(request, 'dash/dash_pro_man.html')
