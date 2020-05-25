c133ff07c4170e9069b40b401cc432eb340abe28
silvermoon06 <silvermoon06000@gmail.com>
2020-05-25 21:17:51 +0100

    add finger id auto genereated method

--- project/apps/pointage/models.py
+++ project/apps/pointage/models.py
@@ -1,25 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 from django.db.models import Count, Q
-
+from random import choice
 
 
 # Create your models here.
 
+
 class EmployeManManager(models.Manager):
     def get_my_employe_stats(self, team_id, user_emp):
-        return super().get_queryset().filter(Q(team_id__name__contains = team_id), ~Q(user = user_emp)).values('team_id').annotate(nb_emp_in=Count(
-        'user', 
-        filter=Q(iwssad=True)),
-        nb_emp_out=Count(
-        'user', 
-        filter=Q(iwssad=False)),
-        nb_emp=Count(
-        'user'
+        return super().get_queryset().filter(Q(team_id__name__contains=team_id), ~Q(user=user_emp)).values('team_id').annotate(nb_emp_in=Count(
+            'user',
+            filter=Q(iwssad=True)),
+            nb_emp_out=Count(
+            'user',
+            filter=Q(iwssad=False)),
+            nb_emp=Count(
+            'user'
         ))
-    
+
     def get_my_employe(self, team_id, user_emp):
-        return super().get_queryset().filter(Q(team_id__name__contains = team_id), ~Q(user = user_emp))
+        return super().get_queryset().filter(Q(team_id__name__contains=team_id), ~Q(user=user_emp))
 
 
 class User(AbstractUser):
@@ -39,23 +40,53 @@ class User(AbstractUser):
 class Employe(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     iwssad = models.BooleanField(default=False)
-    finger_id = models.PositiveSmallIntegerField()
+    finger_id = models.PositiveSmallIntegerField(unique=True, blank=True)
     is_uploaded = models.BooleanField(default=False)
     is_delete = models.BooleanField(default=False)
     birthdate = models.DateField(auto_now=False, blank=True, null=True)
     birthplace = models.CharField(max_length=120, blank=True)
     address = models.CharField(max_length=200, blank=True)
-    phone = models.CharField(max_length=200, blank=True)
+    phone = models.CharField(max_length=200, blank=True,
+                             verbose_name="telephone 1")
     observation = models.CharField(max_length=300, blank=True)
     picture = models.ImageField(null=True, blank=True)
-    team_id = models.ForeignKey('dash.Team', on_delete=models.CASCADE, null=True, blank=True)
+    team_id = models.ForeignKey(
+        'dash.Team', on_delete=models.CASCADE, null=True, blank=True)
 
     objects = models.Manager()
-    manager= EmployeManManager()
+    manager = EmployeManManager()
 
     def __str__(self):
         return self.user.username
 
+    def save(self, *args, **kwargs):
+        if self.pk is None:
+            self.genrate_finger_id()
+
+        super(Employe, self).save(*args, **kwargs)
+
+    def genrate_finger_id(self):
+        # get ids alredy used
+        used_id = self.get_used_id()
+        # a liste of the interval
+        my_list = list(range(2, 128))
+        # a liste with thee possible valeus =interval-alredy used elements
+        possible_values_list = set(my_list)-set(used_id)
+        # print(possible_values_list)
+        # a random ellment from this liste
+        # print(choice(list(possible_values_list)))
+        # set  the value to the finger id attribut
+        self.finger_id = choice(list(possible_values_list))
+
+    def get_used_id(self):
+        my_id_list = [1, ]
+        # get employes
+        employes = Employe.objects.all()
+        # get used finger id
+        for employe in employes:
+            my_id_list.append(employe.finger_id)
+       # print(my_id_list)
+        return my_id_list
 
 
 class Shift(models.Model):
