f82a36135bbc05b652b3c94621c2fda0d0ff564e
silvermoon06 <silvermoon06000@gmail.com>
2020-06-21 14:53:04 +0100

    manager can have more then one team

--- project/apps/dash/views/view_manger.py
+++ project/apps/dash/views/view_manger.py
@@ -3,7 +3,7 @@ from apps.dash.decorators import employe_required, manger_required, responsible_
 from django.contrib.auth.decorators import login_required
 from apps.pointage.models import Employe, User, Planing, Shift, Team
 from django.core.exceptions import ObjectDoesNotExist
-from apps.dash.forms import UserForm
+from apps.dash.forms import UserForm, TeamForm
 from django.urls import reverse
 from django.forms.models import inlineformset_factory
 from django.core.exceptions import PermissionDenied
@@ -12,10 +12,6 @@ import datetime
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from apps.dash.views import get_now_t, convert_time, is_valid, hours_dif, get_laste_entry, get_inpost_t, get_time_left
 from django.db.models import Q
-
-
-
-
 
 
 def get_coleagues(employe):
@@ -59,56 +55,52 @@ def dash_man(request):
     #         print(co)
 
     # the team dashbored part
-    # get the team manged by the actuel employe
-    try :
-        team = Team.objects.filter(manager=man).get()
-        print(team)
-    # get the employes of the team
-    except : 
-        pass
-    if(team):
-        colaborators=get_employes(team)
-        colaborators_all_nbr = colaborators.count()
-        print(colaborators_all_nbr)
-        colaborators_in_nbr = get_employes_by_presence(team, True).count()
-        print(colaborators_in_nbr)
-        colaborators_out_nbr = get_employes_by_presence(team, False).count()
-        print(colaborators_out_nbr)
+    # # get the team manged by the actuel employe
+    # try :
+    #     team = Team.objects.filter(manager=man).get()
+    #     print(team)
+    # # get the employes of the team
+    # except :
+    #     pass
+    # if(team):
+    #     colaborators=get_employes(team)
+    #     colaborators_all_nbr = colaborators.count()
+    #     print(colaborators_all_nbr)
+    #     colaborators_in_nbr = get_employes_by_presence(team, True).count()
+    #     print(colaborators_in_nbr)
+    #     colaborators_out_nbr = get_employes_by_presence(team, False).count()
+    #     print(colaborators_out_nbr)
 
-        male_collabortors_nbr=colaborators.filter(gender='H').count()
-        female_collabortors_nbr=colaborators.filter(gender='F').count()
-        # recapitulatif_présence = [colaborators_in,colaborators_out,colaborators_all]
-        
-    else:
-        colaborators_all_nbr = 0
-        colaborators_in_nbr = 0
-        colaborators_out_nbr = 0
-       
-    # get colabotators 
-    if(colaborators):
-        for col in colaborators:
-            shift=col.get_last_shift()
-            if (shift):
-                col.laste_entry=shift.he
-                col.in_post_time=get_inpost_t(col)
-            else:
-                col.laste_entry=None
-                col.in_post_time=None
-    
+    #     male_collabortors_nbr=colaborators.filter(gender='H').count()
+    #     female_collabortors_nbr=colaborators.filter(gender='F').count()
+    #     # recapitulatif_présence = [colaborators_in,colaborators_out,colaborators_all]
+
+    # else:
+    #     colaborators_all_nbr = 0
+    #     colaborators_in_nbr = 0
+    #     colaborators_out_nbr = 0
+
+    # # get colabotators
+    # if(colaborators):
+    #     for col in colaborators:
+    #         shift=col.get_last_shift()
+    #         if (shift):
+    #             col.laste_entry=shift.he
+    #             col.in_post_time=get_inpost_t(col)
+    #         else:
+    #             col.laste_entry=None
+    #             col.in_post_time=None
+
     # for c in col :
     #     shift=c.get_last_shift()
     #     entryes=entryes.append(get_laste_entry(shift))
     #     print(entryes)
-             
-
-    
 
     # get the number of the employes in the team
     # get the the number of the employes presentes
 
     # return template with context after convert work_time to hours and min
     return render(request, 'dash/dash_man.html', locals())
-   
 
 
 @manger_required
@@ -117,13 +109,13 @@ def mes_collaborateurs(request):
     manger = Employe.objects.filter(user=request.user).get()
     # Get manager team
     try:
-        team = Team.objects.filter( manager = manger).get()
+        team = Team.objects.filter(manager=manger).get()
     except:
         print("No team")
     # if he is the team manager we get all employe of this team except the manager
     try:
         if manger == team.manager:
-            list_emp = Employe.manager.get_my_employe(team,request.user)
+            list_emp = Employe.manager.get_my_employe(team, request.user)
             # Check if request method is GET
             if request.GET:
                 # Get str data from fields nom
@@ -132,35 +124,36 @@ def mes_collaborateurs(request):
                 # Check if not None or ''
                 if is_valid(nom):
                     # Filter list_emp with lastame
-                    list_emp = list_emp.filter(Q(user__last_name__istartswith=nom) | Q(user__first_name__istartswith=nom))
-                    
+                    list_emp = list_emp.filter(Q(user__last_name__istartswith=nom) | Q(
+                        user__first_name__istartswith=nom))
+
                 if is_valid(status):
                     if status == '1':
                         list_emp = list_emp.filter(iwssad=True)
                     else:
                         list_emp = list_emp.filter(iwssad=False)
 
-            return render(request, 'dash/mes_collaborateurs.html', {'employes':list_emp, 'team':True})
+            return render(request, 'dash/mes_collaborateurs.html', {'employes': list_emp, 'team': True})
     except:
         print('except')
-        return render(request, 'dash/mes_collaborateurs.html',{'team':False})
+        return render(request, 'dash/mes_collaborateurs.html', {'team': False})
 
 
-@manger_required   
+@manger_required
 def fiche_pointage(request):
     # Get manager
     manger = Employe.objects.filter(user=request.user).get()
     # Get manager team
     try:
-        team = Team.objects.filter(manager = manger).get()
+        team = Team.objects.filter(manager=manger).get()
     except:
         print("No team")
-    # if he is the team manager we get all employe of this team except the manager 
+    # if he is the team manager we get all employe of this team except the manager
     try:
         if manger == team.manager:
-            list_emp = Employe.manager.get_my_employe(team,request.user)
+            list_emp = Employe.manager.get_my_employe(team, request.user)
             for emp in list_emp:
-                shifts = emp.get_last_shift()           
+                shifts = emp.get_last_shift()
                 if shifts:
                     emp.he = shifts.he
                     emp.hs = shifts.hs
@@ -175,7 +168,8 @@ def fiche_pointage(request):
                 # Check if not None or ''
                 if is_valid(nom):
                     # Filter list_emp with lastame
-                    list_emp = list_emp.filter(Q(user__last_name__istartswith=nom) | Q(user__first_name__istartswith=nom))
+                    list_emp = list_emp.filter(Q(user__last_name__istartswith=nom) | Q(
+                        user__first_name__istartswith=nom))
                 if is_valid(status):
                     if status == '1':
                         list_emp = list_emp.filter(iwssad=True)
@@ -185,30 +179,28 @@ def fiche_pointage(request):
     except:
         list_emp = None
 
-                
-                
-    return render(request, 'dash/fiche_pointage.html',{'shifts':list_emp})
+    return render(request, 'dash/fiche_pointage.html', {'shifts': list_emp})
 
 
-def mon_equipe(request):
+def mes_equipes(request):
     # Get employe with id
     man = Employe.objects.filter(user=request.user).get()
-    # the employe dash boreed part
+    teams = Team.objects.filter(manager=man)
+    teams_nbr = teams.count()
+    print(teams)
 
-    shift = man.get_last_shift()
-    he = get_laste_entry(shift)
-    now = get_now_t()
-    in_post_t = get_inpost_t(man)
-    time_left = get_time_left(man)
-    try:
-        t_h = man.get_today_hours()
-        todays_hours = [t_h.he1, t_h.hs1, t_h.he2, t_h.hs2, ]
-    except:
-        todays_hours = ['H:M', 'H:M', 'H:M', 'H:M']
+    return render(request, 'dash/mes_equipes.html', locals())
+
+
+def equipe_view(request,pk):
+    # Get employe with id
+    man = Employe.objects.filter(user=request.user).get()
+    team = Team.objects.get(id=pk)
+    print(team)
     coleagues = get_coleagues(man)
-    # if(coleagues):
-    #     for co in coleagues:
-    #         print(co)
+    if(coleagues):
+        for co in coleagues:
+            print(co)
 
     # the team dashbored part
     # get the team manged by the actuel employe
@@ -216,7 +208,7 @@ def mon_equipe(request):
         team = Team.objects.filter(manager=man).get()
         print(team)
     # get the employes of the team
-    except : 
+    except :
         pass
     if(team):
         colaborators=get_employes(team)
@@ -230,13 +222,13 @@ def mon_equipe(request):
         male_collabortors_nbr=colaborators.filter(gender='H').count()
         female_collabortors_nbr=colaborators.filter(gender='F').count()
         # recapitulatif_présence = [colaborators_in,colaborators_out,colaborators_all]
-        
+
     else:
         colaborators_all_nbr = 0
         colaborators_in_nbr = 0
         colaborators_out_nbr = 0
-       
-    # get colabotators 
+
+    # get colabotators
     if(colaborators):
         for col in colaborators:
             shift=col.get_last_shift()
@@ -246,18 +238,11 @@ def mon_equipe(request):
             else:
                 col.laste_entry=None
                 col.in_post_time=None
-    
+
     # for c in col :
     #     shift=c.get_last_shift()
     #     entryes=entryes.append(get_laste_entry(shift))
     #     print(entryes)
-             
 
-    
 
-    # get the number of the employes in the team
-    # get the the number of the employes presentes
-    return render(request, 'dash/mon_equipe.html',locals())
-    
-
-    
+    return render(request, 'dash/equipe_view.html', locals())
